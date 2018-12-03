3b42398f05d330b88e2c8ce18ed85d4fbc5b72fc
saurabhmithe <saurabhmithe@gmail.com>
2018-12-02 16:21:38 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view_engine', 'hbs');
+
+// middleware to log requests
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {console.log('Unable to append log.');}
+    })
+    next();
+});
+
+// middleware to notify abour maintenance
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+    next();
+});
+
+// serve static pages
+app.use(express.static(__dirname + '/public'));
+
+// handlebar helper functions
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (str) => {
+    return str.toUpperCase();
+});
+
+// Handler for HTTP GET
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the web page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    //res.send('<h1>Hello Express!<h1>');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000.');
+});
