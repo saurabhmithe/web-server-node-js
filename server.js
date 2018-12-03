4945f4e45133a88e4a31678084ad0c298310b614
saurabhmithe <saurabhmithe@gmail.com>
2018-12-02 16:34:14 -0800

    adding heroku configuration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -50,6 +51,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started at port 3000.');
+app.listen(port, () => {
+    console.log(`Server started at port ${port}.`);
 });
